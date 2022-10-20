18cfe0e3aa1173c399dd37ad87ab136576ddc6dc
Lucas <lucasfreiresabino@gmail.com>
2022-10-20 10:03:42 -0600

    made a function of order to tshirt size

---
+++ index.js
@@ -0,0 +1,15 @@
+const tshirt = (str) => {
+    let array = str.split('')
+
+    let sArray = array.filter(element => element === 's')
+    let mArray = array.filter(element => element === 'm')
+    let lArray = array.filter(element => element === 'l')
+
+    let resulte = [...sArray, ...mArray, ...lArray]
+
+    console.log(resulte)
+
+}
+
+tshirt('smllms')
+tshirt('lms')
